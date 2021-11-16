52d941728cf1a4226848828abbb57ee74a6a85ef
Aventure20 <70507259+Aventure20@users.noreply.github.com>
2021-11-16 09:06:06 +0100

    Translating main in English

--- generator.py
+++ generator.py
@@ -1,13 +1,13 @@
 import random
 character = "AZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbn0123456789&#{([-_ç@?./:!§])}"
-length = int(input("Combien de caractère doit contenir votre mot de passe?\n"))
+length = int(input("Wich length do you want for your password?\n"))
 while True:
     if length < 81:
         break
     else:
-        print("Désolé mais le nombre maximal de caractère que votre mot de passse peut contenir doit être =<80")
-        print("Merci de réésayer:")
-        length = int(input("Combien de caractère doit contenir votre mot de passe?\n"))
+        print("Sorry but the max length for your password is =<80")
+        print("Please try again:")
+        length = int(input("Wich length do you want for your password?\n"))
 password = "".join(random.sample(character, length))
 print(password)
-print("Mot de passe généré avec succé!")
+print("Password generated with succes")
