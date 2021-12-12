456aedc3e4f83522e48b021b275640a1d095e7d9
Aventure20 <70507259+Aventure20@users.noreply.github.com>
2021-12-12 16:05:21 +0100

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,13 +1,13 @@
 import random
 character = "AZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbn0123456789&#{([-_ç@?./:!§])}"
-length = int(input("Wich length do you want for your password?\n"))
+length = int(input("Which length do you want for your password?\n"))
 while True:
     if length < 81:
         break
     else:
         print("Sorry but the max length for your password is =<80")
         print("Please try again:")
-        length = int(input("Wich length do you want for your password?\n"))
+        length = int(input("Which length do you want for your password?\n"))
 password = "".join(random.sample(character, length))
 print(password)
-print("Password generated with succes")
+print("Password generated with success")
